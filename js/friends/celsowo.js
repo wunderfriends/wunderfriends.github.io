1efb7f4573e77caae924b001b01a8d3f8fe921db
Celso Wo <celso.wo@tegra.me>
2017-01-06 12:39:44 -0200

    Empty friends streak

--- js/friends/celsowo.js
+++ js/friends/celsowo.js
@@ -3,6 +3,7 @@ window.friends.push({
   name: 'Celso Wo',
   color: '#E91E23',
   streak: {
+    '2017-01-02': true,
     '2017-01-03': true,
     '2017-01-04': true,
     '2017-01-05': true,
--- js/friends/mikelima.js
+++ js/friends/mikelima.js
@@ -3,8 +3,6 @@ window.friends.push({
   name: 'Mike Lima',
   color: '#8CC665',
   streak: {
-    '2017-01-01': true,
-    '2017-01-03': true,
-    '2017-01-06': true,
+    // '2017-01-01': true,
   }
 });
