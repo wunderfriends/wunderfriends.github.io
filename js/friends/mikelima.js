387fb662b06e1e280863725996232da6bc59c31b
Mike Rodrigues De Lima <mikemajesty2011@gmail.com>
2017-01-10 09:09:44 -0200

    Update mikelima.js

--- js/friends/mikelima.js
+++ js/friends/mikelima.js
@@ -1,7 +1,7 @@
 window.friends = window.friends || [];
 window.friends.push({
   name: 'Mike Lima',
-  color: '#8CC665',
+  color: '#FFB6C1',
   streak: {
     '2017-01-09': true,
     '2017-01-10': true
