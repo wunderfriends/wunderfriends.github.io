86c7fa59a95611c320d6307f50efee1cdce95075
GabrielJacquier <gabrieljacquierme@gmail.com>
2017-01-14 14:24:56 -0200

    Update gabrieljacquier.js

--- js/friends/gabrieljacquier.js
+++ js/friends/gabrieljacquier.js
@@ -8,6 +8,7 @@ window.friends.push({
     '2017-01-10': true,
     '2017-01-11': true,
     '2017-01-12': true,
-    '2017-01-13': "Li e assisti conteúdos  em inglês"
+    '2017-01-13': "Li e assisti conteúdos  em inglês",
+    '2017-01-14': "Conversei e escutei em inglês"
   }
 });
