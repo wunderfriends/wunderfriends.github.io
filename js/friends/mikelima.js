7fa14f44a9b2dc5147d026e627287ca326ea8a44
Mike Rodrigues De Lima <mikemajesty2011@gmail.com>
2017-01-16 23:18:37 -0200

    Update mikelima.js

--- js/friends/mikelima.js
+++ js/friends/mikelima.js
@@ -10,6 +10,7 @@ window.friends.push({
     '2017-01-12': true,
     '2017-01-13': true,
     '2017-01-14': [{action:{ study: "I contribute to my lib with 11 commits.", learnings: "Handle minification of files."}}],
-    '2017-01-15': [{action:{ study: "I contribute to my lib with 27 commits.", learnings: "More about css."}}]
+    '2017-01-15': [{action:{ study: "I contribute to my lib with 27 commits.", learnings: "More about css."}}],
+    '2017-01-16': [{action:{ study: "I studied English for 40 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 2 commits.", learnings: ""}}]
   }
 });
