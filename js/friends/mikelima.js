02848ff2379ac24706bb9336c434dd3c497f40c8
Celso Wo <celsowo@gmail.com>
2017-01-19 17:24:59 -0200

    Update mikelima.js

--- js/friends/mikelima.js
+++ js/friends/mikelima.js
@@ -6,13 +6,12 @@ window.friends.push({
     '2017-01-09': true,
     '2017-01-10': true,
     '2017-01-11': true,
-    '2017-01-11': true,
     '2017-01-12': true,
     '2017-01-13': true,
     '2017-01-14': [{action:{ study: "I contribute to my lib with 11 commits.", learnings: "Handle minification of files."}}],
     '2017-01-15': [{action:{ study: "I contribute to my lib with 27 commits.", learnings: "More about css."}}],
     '2017-01-16': [{action:{ study: "I studied English for 40 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 2 commits.", learnings: ""}}],
-    '2017-01-16': [{action:{ study: "I studied English for 25 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 11 commits.", learnings: ""}}],
-    '2017-01-16': [{action:{ study: "I studied English for 30 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 7 commits.", learnings: ""}}]
+    '2017-01-17': [{action:{ study: "I studied English for 25 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 11 commits.", learnings: ""}}],
+    '2017-01-18': [{action:{ study: "I studied English for 30 minutes..", learnings: ""}}, {action:{ study: "I contribute to my lib with 7 commits.", learnings: ""}}]
   }
 });
