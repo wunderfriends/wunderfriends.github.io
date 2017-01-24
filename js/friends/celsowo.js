54d35740aba8d816f4ba3aa5280f3f2b65d6f41a
Celso Wo <celsowo@gmail.com>
2017-01-24 15:53:24 -0200

    Update celsowo.js

--- js/friends/celsowo.js
+++ js/friends/celsowo.js
@@ -25,5 +25,6 @@ window.friends.push({
     '2017-01-21': 'Levei a dulce na agência estrelas',
     '2017-01-22': 'Trabalhei na lib chocobo date picker do mike',
     '2017-01-23': 'Dei um mini-integrado',
+    '2017-01-24': 'Ajustei as imagens do site do GO',
   }
 });
