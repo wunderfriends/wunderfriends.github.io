8c66afbcf3e2700c79aa5ba036de215713fc034d
Celso Wo <celsowo@gmail.com>
2017-01-26 11:38:01 -0200

    Delete leo.js

--- js/friends/leo.js
+++
@@ -1,8 +0,0 @@
-window.friends = window.friends || [];
-window.friends.push({
-  name: 'Celso Wo',
-  color: '#E91E23',
-  streak: {
-    '2017-01-26': 'Ir ao endócrino; Participar do Webinário de Spring Multi-Tenancy',
-  }
-});
