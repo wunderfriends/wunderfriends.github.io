e134bfc75b45968976083f6bcc5960630feb1b21
Léo Costa <leoeduar@gmail.com>
2017-01-26 10:29:11 -0300

    Initial 26/01

---
+++ js/friends/leo.js
@@ -0,0 +1,8 @@
+window.friends = window.friends || [];
+window.friends.push({
+  name: 'Celso Wo',
+  color: '#E91E23',
+  streak: {
+    '2017-01-26': 'Ir ao endócrino; Participar do Webinário de Spring Multi-Tenancy',
+  }
+});
