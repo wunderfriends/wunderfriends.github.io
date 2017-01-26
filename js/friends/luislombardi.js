0f143cc044c0471f2b5c8ee343d805a070eb42a4
Luis Gustavo N Lombardi <lombardi2008@gmail.com>
2017-01-26 10:39:07 -0200

    Update luislombardi.js

--- js/friends/luislombardi.js
+++ js/friends/luislombardi.js
@@ -6,6 +6,7 @@ window.friends.push({
     //'2017-01-02': 'O que eu fiz',
     '2017-01-23': 'Comecei o wunderfriends; Comecei o desenvolvimento em corona. Ajudei nas tarefas de casa, escrevi um pouco no Medium',
     '2017-01-24': 'Levei o cachorro no petshop',
-    '2017-01-25': 'Fiz a janta; estudei Corona; Comprei os remédios do cachorro'
+    '2017-01-25': 'Fiz a janta; estudei Corona; Comprei os remédios do cachorro',
+    '2017-01-26': 'Agendei a vistoria do Youse'
   }
 });
