0dee0e15cf7740b737652560d8e9c109b0d9fc17
Celso Wo <celsowo@gmail.com>
2017-01-28 12:33:40 -0200

    Update leocosta.js

--- js/friends/leocosta.js
+++ js/friends/leocosta.js
@@ -6,7 +6,7 @@ window.friends.push({
   streak: {
     // '2017-01-02': 'O que eu fiz',
     '2017-01-26': 'Ir ao endócrino; Participar do Webinário de Spring Multi-Tenancy',
-    '2017-01-27': 'Fiz vários exames; Mudei de convênio; Tive uma boa idéia de sftw'
-    '2017-01-28': 'Levei o Arthur no campo e no parque;'
+    '2017-01-27': 'Fiz vários exames; Mudei de convênio; Tive uma boa idéia de sftw',
+    '2017-01-28': 'Levei o Arthur no campo e no parque;',
   }
 });
