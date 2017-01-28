7ff1830cd441f37405a0ba4ecd2fe58df317f586
Jean Vitor <jeanvitor06@gmail.com>
2017-01-28 21:26:12 -0200

    Update jeanvitor.js

--- js/friends/jeanvitor.js
+++ js/friends/jeanvitor.js
@@ -4,7 +4,7 @@ window.friends.push({
   color: '#79909B',
   userGit: 'jeanvitor06',
   streak: {
-     '2017-01-26': 'Olhei a minha lista, participei da iniciação wunderfriends'
+     '2017-01-26': 'Olhei a minha lista, participei da iniciação wunderfriends',
      '2017-01-28': 'Estudei reduce'
   }
 });
