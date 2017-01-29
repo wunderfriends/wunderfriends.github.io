4cafc2f95bb43c7c415bf4fbd79a4c4a4537230d
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-01-29 17:17:05 -0200

    Update samuelfrezza.js

--- js/friends/samuelfrezza.js
+++ js/friends/samuelfrezza.js
@@ -6,6 +6,7 @@ window.friends.push({
   streak: {
     '2017-01-26': 'Entrei pro wunderfriends. Fui na academia',
     '2017-01-27': 'Academia e Integrado',
-    '2017-01-28': 'Curso de MongoDB'
+    '2017-01-28': 'Curso de MongoDB',
+    '2017-01-29': true
   }
 });
