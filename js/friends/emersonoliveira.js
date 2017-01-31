512779cdd18a07b7a718f63ae8b609005d3fa0d9
Celso Wo <celso.wo@tegra.me>
2017-01-31 10:17:37 -0200

    Adicionado Matheus e Emerson

---
+++ js/friends/emersonoliveira.js
@@ -0,0 +1,9 @@
+window.friends = window.friends || [];
+window.friends.push({
+  name: 'Emerson Oliveira',
+  color: '#AB3EA5',
+  userGit: 'Todev3',
+  streak: {
+    // '2017-01-02': 'O que eu fiz',
+  }
+});
---
+++ js/friends/matheusoliveira.js
@@ -0,0 +1,9 @@
+window.friends = window.friends || [];
+window.friends.push({
+  name: 'Emerson Oliveira',
+  color: '#BC4747',
+  userGit: 'Mateus-Oli',
+  streak: {
+    // '2017-01-02': 'O que eu fiz',
+  }
+});
