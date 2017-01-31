f32b676686802ff1f944f2353df34872aff4ad9e
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-01-31 16:09:06 -0200

    Update samuelfrezza.js

--- js/friends/samuelfrezza.js
+++ js/friends/samuelfrezza.js
@@ -4,10 +4,11 @@ window.friends.push({
   color: '#1ebccd',
   userGit: 'samfrezza',
   streak: {
-    '2017-01-26': 'Entrei pro wunderfriends. Fui na academia',
-    '2017-01-27': 'Academia e Integrado',
-    '2017-01-28': 'Curso de MongoDB',
+    '2017-01-26': 'entrei pro wunderfriends. fui na academia',
+    '2017-01-27': 'academia e integrado',
+    '2017-01-28': 'curso de mongodb',
     '2017-01-29': true,
-    '2017-01-30': true
+    '2017-01-30': 'academia',
+    '2017-01-31': 'tirei o visto americano'
   }
 });
