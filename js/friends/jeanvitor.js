6477ceb6d1fd4141e0af0f2aed92882276e4c1af
Jean Vitor <jeanvitor06@gmail.com>
2017-01-31 20:12:17 -0200

    Update jeanvitor.js

--- js/friends/jeanvitor.js
+++ js/friends/jeanvitor.js
@@ -4,9 +4,9 @@ window.friends.push({
   color: '#79909B',
   userGit: 'jeanvitor06',
   streak: {
-     '2017-01-26': 'Olhei a minha lista, participei da iniciação wunderfriends',
-     '2017-01-28': 'Estudei reduce'
-     '2017-01-30': 'Estudei o lib exceljs, e ES6',
-     '2017-01-31': 'Instalei o wundercast-web'
+    '2017-01-26': 'Olhei a minha lista, participei da iniciação wunderfriends',
+    '2017-01-28': 'Estudei reduce',
+    '2017-01-30': 'Estudei o lib exceljs, e ES6',
+    '2017-01-31': 'Instalei o wundercast-web'
   }
 });
