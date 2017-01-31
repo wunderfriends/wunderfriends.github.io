ac576b0e4d0bdb426df73648d91a22e5fc461dd6
Emerson <emerson_abedias@hotmail.com>
2017-01-31 16:09:48 -0200

    Update emersonoliveira.js

--- js/friends/emersonoliveira.js
+++ js/friends/emersonoliveira.js
@@ -1,9 +1,9 @@
 window.friends = window.friends || [];
 window.friends.push({
-  name: 'Emerson Oliveira',
+  name: 'Emerson Abdias',
   color: '#AB3EA5',
   userGit: 'Todev3',
   streak: {
-    // '2017-01-02': 'O que eu fiz',
+    '2017-01-02': 'Entrei pro Wunderfriends',
   }
 });
