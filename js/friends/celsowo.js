a4307fef01d16f2ac501d81b6194beec40e9d603
Celso Wo <celsowo@gmail.com>
2017-02-02 20:07:04 -0200

    Update celsowo.js

--- js/friends/celsowo.js
+++ js/friends/celsowo.js
@@ -35,5 +35,6 @@ window.friends.push({
     '2017-01-30': 'Cobrei o gabriel do violão',
     '2017-01-31': 'Criado os arquivos do emerson e matheus',
     '2017-02-01': 'Aprovei o pullrequest do Felipe',
+    '2017-02-02': 'Testei a VPN do Novaera',
   }
 });
