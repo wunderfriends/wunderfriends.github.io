404936d8850f3bf65414cdedec6054f6b73d1239
Emerson <emerson_abedias@hotmail.com>
2017-02-01 23:21:17 -0200

    Update emersonoliveira.js

--- js/friends/emersonoliveira.js
+++ js/friends/emersonoliveira.js
@@ -5,5 +5,6 @@ window.friends.push({
   userGit: 'Todev3',
   streak: {
     '2017-01-31': 'Entrei pro Wunderfriends',
+    '2017-02-31': 'Visitei time da Tegra no Assai. Aprendi mais sobre o Horus',
   }
 });
