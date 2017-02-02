0c404d4ff40761af4e4e5b7e7059c52b11d6eb05
Luis Gustavo N Lombardi <lombardi2008@gmail.com>
2017-02-01 23:00:22 -0200

    Update luislombardi.js

--- js/friends/luislombardi.js
+++ js/friends/luislombardi.js
@@ -12,6 +12,7 @@ window.friends.push({
     '2017-01-27': 'Cobrei o despachante',
     '2017-01-28': 'Visitei os sogros; banho no cachorro; cortei o cabelo (ou o que sobrou dele).',
     '2017-01-30': 'Cobrei o despachante; finalizei uma task importante',
-    '2017-01-31': 'Levei a Ravena no retorno; Comprei os remédios da Ravena; Comprei DVD virgem'
+    '2017-01-31': 'Levei a Ravena no retorno; Comprei os remédios da Ravena; Comprei DVD virgem',
+    '2017-02-01': 'Levei o carro na vistoria; finalizei a formatacao do Notebook'
   }
 });
