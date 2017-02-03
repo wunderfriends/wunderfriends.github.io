564f73a1275e790ece5f135201cad05f338672c0
GabrielJacquier <gabrieljacquierme@gmail.com>
2017-02-03 17:03:39 -0200

    Create luiscarvalho.js

---
+++ js/friends/luiscarvalho.js
@@ -0,0 +1,9 @@
+window.friends = window.friends || [];
+window.friends.push({
+  name: 'Luis Carvalho',
+  color: '#000',
+  userGit: 'jrluiscarvalho',
+  streak: {
+    '2017-02-03': "Entrei no wunderfriends"
+  }
+});
