f5f559fea4117cc181b4dd78066a5527890e09e8
Felipe Bernardes <bernardes.flp@gmail.com>
2017-02-02 23:21:44 -0200

    Update felipebernades.js

--- js/friends/felipebernades.js
+++ js/friends/felipebernades.js
@@ -25,6 +25,7 @@ window.friends.push({
     '2017-01-29': 'revisei meus to-dos da semana e todas as minhas anotaçes',
     '2017-01-30': 'cuidei da minha saúde, hackerhank && comecei a talk de gulp',
     '2017-01-31': true,
-    '2017-02-01': 'matei alguns to-dos de saúde e atualizei outros'
+    '2017-02-01': 'matei alguns to-dos de saúde e atualizei outros',
+    '2017-02-02': 'studied tests'
   }
 });
