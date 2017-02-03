2afb174dbe1a0cca6dab518e3c1f00283ad8aa71
Jean Vitor <jeanvitor06@gmail.com>
2017-02-03 20:08:16 -0200

    Update jeanvitor.js

--- js/friends/jeanvitor.js
+++ js/friends/jeanvitor.js
@@ -8,7 +8,7 @@ window.friends.push({
     '2017-01-28': 'Estudei reduce',
     '2017-01-30': 'Estudei o lib exceljs, e ES6',
     '2017-01-31': 'Instalei o wundercast-web',
-    '2017-02-01': 'Comecei a escrever um artigo no medium e rodei o projeto randsome_meetup'
+    '2017-02-01': 'Comecei a escrever um artigo no medium e rodei o projeto randsome_meetup',
     '2017-03-01': 'Escrevi um artigo no medium'
   }
 });
