8defc278c5ca22084cbc907754e64be0480447cc
Emerson <emerson_abedias@hotmail.com>
2017-02-04 23:12:31 -0200

    Update emersonoliveira.js

--- js/friends/emersonoliveira.js
+++ js/friends/emersonoliveira.js
@@ -7,5 +7,7 @@ window.friends.push({
     '2017-01-31': 'Entrei pro Wunderfriends',
     '2017-02-01': 'Visitei time da Tegra no Assai. Aprendi mais sobre o Horus',
     '2017-02-02': 'Postei um texto sobre alocacao dinamica.',
+    '2017-02-03': 'Discussao sobre JS e Java.',
+    '2017-02-04': 'Sai para fazer nada.',
   }
 });
