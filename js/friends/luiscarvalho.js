ced2a235252f739b7cc951d29d1b09c100ec5459
jrluiscarvalho <jrluiscarvalho@gmail.com>
2017-02-07 23:21:19 -0200

    Update luiscarvalho.js

--- js/friends/luiscarvalho.js
+++ js/friends/luiscarvalho.js
@@ -5,6 +5,7 @@ window.friends.push({
   userGit: 'jrluiscarvalho',
   streak: {
     '2017-02-03': "Entrei no wunderfriends",
-    '2017-02-06': "Estudei upload de arquivos com express"
+    '2017-02-06': "Estudei upload de arquivos com express",
+    '2017-02-07': "Estudei map, filter, forEach e Promises"
   }
 });
