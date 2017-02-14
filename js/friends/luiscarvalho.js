9fb35f63ff29cd3285fb1b57c7bad94b18ca42c9
jrluiscarvalho <jrluiscarvalho@gmail.com>
2017-02-13 23:40:52 -0200

    Update luiscarvalho.js

--- js/friends/luiscarvalho.js
+++ js/friends/luiscarvalho.js
@@ -7,6 +7,7 @@ window.friends.push({
     '2017-02-03': "Entrei no wunderfriends",
     '2017-02-06': "Estudei upload de arquivos com express",
     '2017-02-07': "Estudei map, filter, forEach e Promises",
-    '2017-02-08': "Promises e reduce no Alura"
+    '2017-02-08': "Promises e reduce no Alura",
+    '2017-02-13': "Estudei node no BeMean"
   }
 });
