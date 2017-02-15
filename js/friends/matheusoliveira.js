ffa960b5221bdba117887c4e7d7781910dc64360
Mateus-Oli <mateus.oli.car@gmail.com>
2017-02-15 03:27:47 -0200

    Update matheusoliveira.js

--- js/friends/matheusoliveira.js
+++ js/friends/matheusoliveira.js
@@ -7,6 +7,7 @@ window.friends.push({
     '2017-01-31': 'Conheci o wunderfriends',
     '2017-02-04': true,
     '2017-02-08': true,
-    '2017-02-09': true
+    '2017-02-09': true,
+    '2017-02-15': 'Estudei Scheme'
   }
 });
