27afd11eb1ee7d654a5483faa78c08cd242cb368
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-02-17 15:47:27 -0200

    Update samuelfrezza.js

--- js/friends/samuelfrezza.js
+++ js/friends/samuelfrezza.js
@@ -25,6 +25,7 @@ window.friends.push({
     '2017-02-13': 'academia',
     '2017-02-14': true,
     '2017-02-15': 'academia',
-    '2017-02-16': true
+    '2017-02-16': true,
+    '2017-02-17': true
   }
 });
