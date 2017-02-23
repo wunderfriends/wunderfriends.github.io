dc209503629338acb37fee753e189affb7269754
Samuel Frezza Pisa <samfrezza@gmail.com>
2017-02-23 11:26:28 -0300

    Update samuelfrezza.js

--- js/friends/samuelfrezza.js
+++ js/friends/samuelfrezza.js
@@ -31,6 +31,7 @@ window.friends.push({
     '2017-02-19': 'fiz lasanha',
     '2017-02-20': 'academia',
     '2017-02-21': 'academia',
-    '2017-02-22': true
+    '2017-02-22': true,
+    '2017-02-23': true
   }
 });
