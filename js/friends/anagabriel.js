52363263df20df7823e5631e8491fcc98d20a4ef
Felipe Bernardes <bernardes.flp@gmail.com>
2017-02-25 17:37:07 -0300

    Create anagabriel.js

---
+++ js/friends/anagabriel.js
@@ -0,0 +1,9 @@
+window.friends = window.friends || [];
+window.friends.push({
+  name: 'Ana Gabriel',
+  color: '#4b0082',
+  userGit: 'anaagabriel',
+  streak: {
+    '2017-02-25': 'iniciou o wunderfriends'
+  }
+});
