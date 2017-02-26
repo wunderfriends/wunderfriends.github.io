0c4e0765ec8cabf275e6ef5efa9c8c45638cd3b5
ana_gabriel <ana.gabriel@tegra.me>
2017-02-26 15:00:55 -0300

    Update anagabriel.js

--- js/friends/anagabriel.js
+++ js/friends/anagabriel.js
@@ -4,6 +4,7 @@ window.friends.push({
   color: '#4b0082',
   userGit: 'anaagabriel',
   streak: {
-    '2017-02-25': 'iniciou o wunderfriends'
+    '2017-02-25': 'iniciou o wunderfriends',
+    '2017-02-26': 'Tomei café da manhã especial na Sabina; Fiz a compra da semana; Criei minha lista',
   }
 });
