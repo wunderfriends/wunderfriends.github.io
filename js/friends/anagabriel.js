53c43d2ad05d1f1e6246849b61b13f909d6e3988
ana_gabriel <ana.gabriel@tegra.me>
2017-03-01 19:30:42 -0300

    Update anagabriel.js

--- js/friends/anagabriel.js
+++ js/friends/anagabriel.js
@@ -8,5 +8,6 @@ window.friends.push({
     '2017-02-26': 'Tomei café da manhã especial na Sabina; Fiz a compra da semana; Criei minha lista',
     '2017-02-27': 'Li dois capítulos do livro O poder do hábito; Trabalhei um pouco; Testei o software Bizagi; Caminhei 1 hora',
     '2017-02-28': 'Vi alguns vídeos sobre programas de intercâmbio para os EUA; Estudei processos; Trabalhei',
+    '2017-03-01': true,
   }
 });
