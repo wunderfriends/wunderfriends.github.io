3a77371d63686db32c58fddad836c9b8951d55aa
Celso Wo <celsowo@gmail.com>
2017-05-14 21:19:30 -0300

    Update celsowo.js

--- js/friends/celsowo.js
+++ js/friends/celsowo.js
@@ -131,5 +131,6 @@ window.friends.push({
     '2017-05-11': 'Treino de pernas - treino médio, com fraqueza',
     '2017-05-12': 'Reunião com o Cicero',
     '2017-05-13': 'Levei a dulce pra tomar vacina da gripe. Treino de peito/costa, senti melhoras na contração do peito',
+    '2017-05-14': 'Treino de ombro/braços, médio, faltou energia',
   }
 });
